9518ae1a710b2fcceb6ffc2b5be93f32c709a5fc
Jessica Mann <jmann@najmann.local>
2015-06-16 17:28:35 -0500

    began List books story

---
+++ src/com/twu/biblioteca/Library.java
@@ -0,0 +1,7 @@
+package com.twu.biblioteca;
+
+/**
+ * Created by jmann on 6/16/15.
+ */
+public class Library {
+}
---
+++ test/com/twu/biblioteca/LibraryTest.java
@@ -0,0 +1,21 @@
+package com.twu.biblioteca;
+
+
+import org.junit.Test;
+
+import java.io.PrintStream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class LibraryTest {
+
+    @Test
+    public void shouldListBooksAfterWelcomeMessage(){
+        Library biblioteca = new Library();
+
+    }
+}
