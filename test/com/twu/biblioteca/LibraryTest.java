fcd6b823f97e22bea0a2637353fb0cbada7bbb5a
jessicamann <jmann1163@gmail.com>
2015-06-17 09:32:28 -0500

    Refactored LibraryTest (added @Before method).

--- test/com/twu/biblioteca/LibraryTest.java
+++ test/com/twu/biblioteca/LibraryTest.java
@@ -1,41 +1,47 @@
 package com.twu.biblioteca;
 
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 public class LibraryTest {
 
+
+    PrintStream printStream;
+    List<String> books;
+    Library library;
+
+    @Before
+    public void setup() {
+
+        printStream = mock(PrintStream.class);
+        books = new ArrayList<>();
+        library = new Library(printStream, books);
+
+    }
+
     @Test
     public void shouldListNoBooksWhenThereIsNoBook(){
-        PrintStream printStream = mock(PrintStream.class);
-        List<String> books = new ArrayList<>();
-
-        Library library = new Library(printStream, books);
-
         library.listBooks();
 
-        verify(printStream).println("");
+        verify(printStream, never()).println(anyString());
     }
 
     @Test
     public void shouldListTheBookNameWhenThereIsOneBook(){
-        PrintStream printStream = mock(PrintStream.class);
-        List<String> books = new ArrayList<>();
         books.add("Harry Potter");
-        Library library = new Library(printStream, books);
+
         library.listBooks();
 
         verify(printStream).println("Harry Potter");
-
     }
 }
